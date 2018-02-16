302f808c7912e6def88c4085ea26d188f6b6b299
BBOuser <BBOuser@RIO.local>
2018-02-16 14:35:21 +0000

    Added functionality to admin panel

--- src/pages/admin/admin.ts
+++ src/pages/admin/admin.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NgSwitch, NgSwitchDefault,NgSwitchCase} from '@angular/common';
+
 
 @Component({
   selector: 'page-admin',
   templateUrl: 'admin.html'
 })
 export class AdminPage {
-
+    role:string='navigators'
   constructor(public navCtrl: NavController) {
 
   }
