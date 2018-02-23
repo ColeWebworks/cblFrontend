fc090467a2c94d9dedfd9e1636c70e71ef2ad527
Kevin Bowler <kpbowler@gmail.com>
2018-02-23 11:49:33 +0000

    Updates to the API communication.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import{
+  Http,
+  HttpModule
+} from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AuthService } from '../services/login.service';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
@@ -19,6 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
@@ -31,6 +40,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    AuthService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
